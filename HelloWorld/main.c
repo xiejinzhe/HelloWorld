a3cffa815bd6408e5b15a032cf2341e9d23ad28e
P X <xiejinzhe@bogon.lan>
2013-04-08 21:09:23 +0800

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by P X on 13-4-8.
+//  Copyright (c) 2013年 P X. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
